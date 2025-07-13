640282c012d84a477a1571ede8658493733c68f8
pluto476526 <ngugikibuka99@gmail.com>
2025-07-13 11:29:11 +0300

    auto-commit [2025-07-13 11:29:11]

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,19 +1,20 @@
 import sqlite3
 import functools
+from datetime import datetime
 
-# Decorator to log SQL queries
+# Decorator to log SQL queries with timestamp
 def log_queries():
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             query = kwargs.get("query") or (args[0] if args else "")
-            print(f"Executing SQL query: {query}")
+            print(f"[{datetime.now()}] Executing SQL query: {query}")
             return func(*args, **kwargs)
         return wrapper
     return decorator
 
 
-@log_queries()
+@log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
