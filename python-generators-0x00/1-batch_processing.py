594d6f8aedfd339dbe46e57b64f29d9a6a6c423e
pluto476526 <ngugikibuka99@gmail.com>
2025-07-13 09:46:38 +0300

    auto-commit [2025-07-13 09:46:38]

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -18,16 +18,16 @@ def stream_users_in_batches(batch_size):
             yield batch
             batch = []
     if batch:
-        yield batch
+        yield batch  # yield final incomplete batch
 
     cursor.close()
     connection.close()
 
 
 def batch_processing(batch_size):
-    """Processes each batch to yield users over age 25."""
+    """Generator that yields users over age 25, batch by batch."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
-                print(user)
+                yield user
 
