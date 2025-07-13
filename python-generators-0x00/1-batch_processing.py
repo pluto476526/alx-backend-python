8b58e4e3655e77bfa833d3b5b5703005a5645465
pluto476526 <ngugikibuka99@gmail.com>
2025-07-13 09:47:51 +0300

    auto-commit [2025-07-13 09:47:51]

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 def stream_users_in_batches(batch_size):
-    """Generator that yields rows from user_data table in batches."""
+    """Generator that yields batches of users from the user_data table."""
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -17,15 +17,16 @@ def stream_users_in_batches(batch_size):
         if len(batch) == batch_size:
             yield batch
             batch = []
+
     if batch:
-        yield batch  # yield final incomplete batch
+        yield batch
 
     cursor.close()
     connection.close()
 
 
 def batch_processing(batch_size):
-    """Generator that yields users over age 25, batch by batch."""
+    """Generator that yields users over age 25 from batches."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
