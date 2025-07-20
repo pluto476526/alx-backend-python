8ae904b54f51b8296b11e0268449e8797fcfa75b
pluto476526 <ngugikibuka99@gmail.com>
2025-07-20 16:43:52 +0300

    auto-commit [2025-07-20 16:43:52]

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,7 +27,7 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that access_nested_map raises KeyError for invalid paths."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), repr(path[len(context.exception.args[0]) - 1]))
+        self.assertEqual(str(context.exception), repr(path[-1]))
 
 
 class TestGetJson(unittest.TestCase):
