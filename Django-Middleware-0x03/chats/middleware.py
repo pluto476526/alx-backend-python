0d75e219192fa80d2fbe2490e976a33cbcd3e8f0
pluto476526 <ngugikibuka99@gmail.com>
2025-07-27 15:05:49 +0300

    auto-commit [2025-07-27 15:05:49]

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,22 @@
+# chats/middleware.py
+
+import logging
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        logging.basicConfig(
+            filename='requests.log',
+            level=logging.INFO,
+            format='%(message)s'
+        )
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logging.info(log_entry)
+
+        response = self.get_response(request)
+        return response
+
