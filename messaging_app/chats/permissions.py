cd9439e232081c6c0261961b01b9d0d5722b7d22
pluto476526 <ngugikibuka99@gmail.com>
2025-07-27 13:54:58 +0300

    auto-commit [2025-07-27 13:54:58]

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipant(BasePermission):
+class IsParticipant(permissions.BasePermission):
     """Allow access only if user is a participant in the conversation"""
 
     def has_object_permission(self, request, view, obj):
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -107,6 +107,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  # Default permission
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
