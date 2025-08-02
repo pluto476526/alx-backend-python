fb33abcf5f0b90d1d5e430212bb05dabc02f1be3
pluto476526 <ngugikibuka99@gmail.com>
2025-08-02 22:49:06 +0300

    auto-commit [2025-08-02 22:49:06]

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,10 +1,14 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import Message, Notification, MessageHistory
+from django.utils import timezone
+
+User = get_user_model()
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
-    if created:
+    if created and instance.receiver:  # Check if receiver exists
         Notification.objects.create(
             user=instance.receiver,
             message=instance
@@ -25,3 +29,15 @@ def track_message_edit(sender, instance, **kwargs):
                 instance.last_edited = timezone.now()
         except Message.DoesNotExist:
             pass  # New message, nothing to compare
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    # Clean up messages where user was sender or receiver
+    Message.objects.filter(sender=instance).update(sender=None)
+    Message.objects.filter(receiver=instance).update(receiver=None)
+    
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
+    
+    # Clean up message history where user was editor
+    MessageHistory.objects.filter(edited_by=instance).update(edited_by=None)
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+User = get_user_model()
+
+def delete_user(request):
+    user = request.user
+    user.delete()
+    messages.success(request, "Your account has been deleted.")
+    return redirect('home')  # Replace 'home' with your actual home URL name
