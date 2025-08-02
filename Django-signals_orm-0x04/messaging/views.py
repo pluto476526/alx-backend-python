2fe26a4eef05b9dbf7f0ca45e9434bb4480cb753
pluto476526 <ngugikibuka99@gmail.com>
2025-08-02 23:01:59 +0300

    auto-commit [2025-08-02 23:01:59]

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,8 +3,35 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.decorators.http import require_POST
+from .models import Message
+
 
 User = get_user_model()
+
+@login_required
+def conversation_view(request, user_id):
+    receiver = get_object_or_404(User, pk=user_id)
+    sender = request.user
+    messages = Message.objects.filter(
+        Q(sender=sender, receiver=receiver) |
+        Q(sender=receiver, receiver=sender)
+    ).select_related('sender', 'receiver', 'parent_message').order_by('timestamp')
+    
+    return render(request, 'messaging/conversation.html', {
+        'receiver': receiver,
+        'messages': messages
+    })
+
+
+@login_required
+def thread_view(request, message_id):
+    root_message = get_object_or_404(Message, pk=message_id)
+    thread = Message.objects.get_thread(root_message)
+    return render(request, 'messaging/thread.html', {
+        'root_message': root_message,
+        'thread': thread
+    })
+
 
 def delete_user(request):
     user = request.user
